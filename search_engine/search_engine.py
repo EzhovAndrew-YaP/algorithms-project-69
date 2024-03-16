bf3fdda30a064d7afa9d754fe1948a0807b596df
Andrei Ezhov <mailforinternetreg@gmail.com>
2024-03-16 20:57:44 +0300

    step 3

--- search_engine/search_engine.py
+++ search_engine/search_engine.py
@@ -1,12 +1,24 @@
 import re
 
 
+def delete_punctuation(word: str) -> str:
+    return "".join(re.findall(r"\w+", word)).lower()
+
+
 def search(docs: list[dict[str, str]], word: str) -> list[str]:
     result = []
-    term = "".join(re.findall(r"\w+", word)).lower()
+    rank = []
+    term = delete_punctuation(word)
     for doc in docs:
-        if term not in doc["text"]:
+        words = list(map(delete_punctuation, doc["text"].split()))
+        match_count = len(list(filter(lambda x: x == term, words)))
+        if match_count == 0:
             continue
 
         result.append(doc["id"])
+        rank.append(match_count)
+        i = len(rank) - 2
+        while i >= 0 and rank[i] < match_count:
+            rank[i], rank[i + 1] = rank[i + 1], rank[i]
+            result[i], result[i + 1] = result[i + 1], result[i]
     return result
--- tests/test_search.py
+++ tests/test_search.py
@@ -8,12 +8,23 @@ def test_search_empty_collection():
 def test_search_find_exists():
     assert search(
         [
-            {"id": "doc1", "text": "I am new hexlet boy"},
-            {"id": "doc2", "text": "Is hexlet a programmers site?"},
+            {"id": "doc1", "text": "I am new hex let boy"},
+            {"id": "doc2", "text": "Is hex let a programmers site?"},
             {"id": "doc3", "text": "small cow says moooooo"},
         ],
         "hex",
     ) == ["doc1", "doc2"]
+
+
+def test_only_one_match():
+    assert search(
+        [
+            {"id": "doc1", "text": "I am new hex let boy"},
+            {"id": "doc2", "text": "Is green let a programmers site?"},
+            {"id": "doc3", "text": "small cow says moooooo"},
+        ],
+        "hex",
+    ) == ["doc1"]
 
 
 def test_search_no_match():
@@ -33,8 +44,8 @@ def test_search_no_match():
 def test_search_with_punctuation():
     assert search(
         [
-            {"id": "doc1", "text": "I am new hexlet boy"},
-            {"id": "doc2", "text": "Is hexlet a programmers site?"},
+            {"id": "doc1", "text": "I am new hex let boy"},
+            {"id": "doc2", "text": "Is hex let a programmers site?"},
             {"id": "doc3", "text": "small cow says moooooo"},
         ],
         "hex!",
@@ -42,9 +53,23 @@ def test_search_with_punctuation():
 
     assert search(
         [
-            {"id": "doc1", "text": "I am new hexlet boy"},
-            {"id": "doc2", "text": "Is hexlet a programmers site?"},
+            {"id": "doc1", "text": "I am new hex let boy"},
+            {"id": "doc2", "text": "Is hex let a programmers site?"},
             {"id": "doc3", "text": "small cow says moooooo"},
         ],
         ";hex",
     ) == ["doc1", "doc2"]
+
+
+def test_search_right_rank():
+    doc1 = "I can't shoot straight unless I've had a pint!"
+    doc2 = "Don't shoot shoot shoot that thing at me."
+    doc3 = "I'm your shooter."
+
+    docs = [
+        {"id": "doc1", "text": doc1},
+        {"id": "doc2", "text": doc2},
+        {"id": "doc3", "text": doc3},
+    ]
+
+    assert search(docs, "shoot") == ["doc2", "doc1"]

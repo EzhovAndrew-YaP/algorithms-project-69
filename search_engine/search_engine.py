234fa0b1f45c65ea4b4afbac719efb4126368e9d
Andrei Ezhov <mailforinternetreg@gmail.com>
2024-03-16 21:20:05 +0300

    step 4

--- search_engine/search_engine.py
+++ search_engine/search_engine.py
@@ -5,20 +5,38 @@ def delete_punctuation(word: str) -> str:
     return "".join(re.findall(r"\w+", word)).lower()
 
 
-def search(docs: list[dict[str, str]], word: str) -> list[str]:
+def get_match_stat(terms: list[str], words: list[str]) -> tuple[int, int]:
+    matched_set = set()
+    matched_words_num, all_matches_num = 0, 0
+    for word in words:
+        if word not in terms:
+            continue
+
+        all_matches_num += 1
+
+        if word not in matched_set:
+            matched_words_num += 1
+            matched_set.add(word)
+    return matched_words_num, all_matches_num
+
+
+def search(docs: list[dict[str, str]], sentence: str) -> list[str]:
     result = []
     rank = []
-    term = delete_punctuation(word)
+    terms = list(map(delete_punctuation, sentence.split()))
     for doc in docs:
         words = list(map(delete_punctuation, doc["text"].split()))
-        match_count = len(list(filter(lambda x: x == term, words)))
-        if match_count == 0:
+        matched_words_num, all_matches_num = get_match_stat(terms, words)
+        if matched_words_num == 0:
             continue
 
         result.append(doc["id"])
-        rank.append(match_count)
+        rank.append((matched_words_num, all_matches_num))
         i = len(rank) - 2
-        while i >= 0 and rank[i] < match_count:
+        while i >= 0 and (
+            rank[i][0] < matched_words_num
+            or (rank[i][0] == matched_words_num and rank[i][1] < all_matches_num)
+        ):
             rank[i], rank[i + 1] = rank[i + 1], rank[i]
             result[i], result[i + 1] = result[i + 1], result[i]
     return result
